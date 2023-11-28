ba37312c0d6f065fde852d4e128b41196cdc81ca
cemmertsimsek <cemsimseksm@gmail.com>
2023-11-28 16:17:39 +0300

    switch case

---
+++ uygulama-icerigi/JS/04-switch-case.js
@@ -0,0 +1,3 @@
+// Kullanıcıdan bir ay ismi alın ve girilen ayın kaç gün olduğunu konsola yazdırın.
+
+// BMW, Mercedes ve Audi Markaları için farklı renk çeşitlerine göre stok durumunu kontrol eden iç içe bir switch yapısı oluşturun. Marka ve renk bilgilerini prompt üzerinden alınız. yaratacağınız stokDurumu değişkenini stokta "Var" veya "Yok" olarak güncellesin.
