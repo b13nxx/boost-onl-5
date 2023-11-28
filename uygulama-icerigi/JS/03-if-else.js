817ea94f7c31354aae3f62a8bf3f314ad6356939
cemmertsimsek <cemsimseksm@gmail.com>
2023-11-28 15:04:01 +0300

    if else

---
+++ uygulama-icerigi/JS/03-if-else.js
@@ -0,0 +1,6 @@
+// Bir kelimeyi tersine çevirerek farklı bir değişkene atayın. Orijinal kelimenin palindrom (tersine de aynı olan) olup olmadığını kontrol eden bir if else yapısı oluşturun. Örneğin, "kayak" kelimesi palindromdur, ancak "merhaba" palindrom değildir.
+
+// Kullanıcıdan temsili bir oy verme ve aday olma durum sorgusu yapmak için yaş ve uyruk bilgisi alın. Aşağıdaki koşulları uygulayan bir if sorgusu yazın.
+// a- Türk vatandaşı ve 30 yaşından büyük ise aday olabilir.
+// b- Türk vatandaşı ve 18 yaşından büyük ise oy kullanabilir.
+// c- Türk vatandaşı değilse ancak 18 yaşından büyük ise aday olamaz ama oy verebilir.
