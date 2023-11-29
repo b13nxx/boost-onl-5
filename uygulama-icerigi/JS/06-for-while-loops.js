5113b92c0c608c12db9ee7d2528cbc431cc8b037
cemmertsimsek <cemsimseksm@gmail.com>
2023-11-29 15:31:29 +0300

    for while loops

---
+++ uygulama-icerigi/JS/06-for-while-loops.js
@@ -0,0 +1,15 @@
+// -------------------For - Başlangıç Bitiş Arası Sayıların Toplamı-----------------------
+// kullanıcıdan bir başlangıç sayısı bir de bitiş sayısı isteyin. Bir for döngüsü yaratarak başlangıç sayısından bitiş sayısına kadar bütün sayıları toplayın. toplamı ekrana yazdırın (başlangıç ve bitiş sayısı dahil)
+
+// -------------------For - Dersi Geçen Öğrenci Sayısı-----------------------
+// Bir sınıfta 5 öğrenci olduğunu düşünelim. Öğrencilerin notlarını almak için prompt kullanarak her öğrenci için bir not girişi yapın. Ardından, bu notları kullanarak sınıf ortalamasını hesaplayın. Son olarak, sınıftaki öğrencilerin kaç tanesinin geçtiğini (geçer not 60) bulun ve konsola yazdırın.
+
+// -------------------While - Sayı Tahmin uygulaması-----------------------
+// Random bir sayı tanımlayın ve kullanıcıdan 1-10 arası bir sayı girerek sayıyı tahmin etmesini isteyin. Toplam 3 hak tanımlayın. While döngüsü kullanarak, kullanıcının hakları bitene kadar denemesine izin verin. Her denemenin sonunda sayıyı arttırıp azaltması gerektiği bilgisini ekrana yazdırın.
+
+// -------------------While - Alışveriş Sepeti Uygulaması-----------------------
+// Kurallar:
+// While döngüsü kullanılacak.
+// Kullanıcı prompta herhangi bir ürün ismi veya "onayla" yazabilir.
+// Ürün ismi yazarsa kullanıcıdan ürünün fiyat bilgisini isteyin ve toplam fiyata ekleyin.
+// Kullanıcı "onayla" yazarsa, ekrana sepetin toplam tutar bilgisini yazdırın.
