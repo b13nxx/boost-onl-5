8959a8753416031839dbe651ee384d1ad0670e8a
cemmertsimsek <cemsimseksm@gmail.com>
2023-11-30 14:08:39 +0300

    functions

---
+++ uygulama-icerigi/JS/07-functions.js
@@ -0,0 +1,14 @@
+// Yazacağınız fonksiyonlar içerisinde console.log ifadesi yer almayacak.
+// 1- Verilen iki sayının çarpımını döndüren bir fonksiyon yazın. (Arrow Function ile)
+// 2- Verilen bir cümlenin kelime sayısını döndüren bir arrow fonksiyon yazın.
+// 3- Verilen bir sayının faktöriyelini hesaplayan bir fonksiyon yazın. (Function Declaration Yöntemi ile)
+// 4- Verilen bir sayının asal olup olmadığını kontrol eden bir fonksiyon yazın. (Function Expression Yöntemi ile)
+// 5- Verilen bir dizi içindeki en büyük sayıyı bulan bir fonksiyon yazın.
+// 6- Verilen bir dizi içindeki sayıların toplamını hesaplayan bir fonksiyon yazın.
+// 7-Yukarıdaki son iki fonksiyonu arrow function olarak yeniden yazın.
+// Buradan itibaren yalnızca arrow function kullanıyoruz.
+// 8- Verilen bir stringi tersine çeviren bir fonksiyon yazın.
+// 9- Verilen bir dizinin elemanlarını rastgele karıştıran bir fonksiyon yazın.
+// 10- Verilen bir sayının pozitif bölenlerini bulan bir fonksiyon yazın.(örn. 12'nin pozitif bölenleri 1,2,3,4,6,12)
+// 11- Verilen bir stringin içinde belirli bir karakterin kaç kez tekrarlandığını hesaplayan bir fonksiyon yazın.
+// 12 - içerisine kaç adet sayı yazılırsa yazılsın, sayıların toplamını döndüren bir fonksiyon yazınız.
