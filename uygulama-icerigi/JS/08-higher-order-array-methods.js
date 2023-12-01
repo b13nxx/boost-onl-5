afc050074278798412129c18be9f80a5a508789a
cemmertsimsek <cemsimseksm@gmail.com>
2023-12-01 15:08:33 +0300

    higher order array methods

--- uygulama-icerigi/JS/08-higher-order-array-methods.js
+++ uygulama-icerigi/JS/08-higher-order-array-methods.js
@@ -1,19 +1,38 @@
-// Verilen bir dizideki elemanları büyük harf yaparak yeni bir dizi oluşturan bir map döngüsü oluşturun.
+// // Verilen bir dizideki elemanları büyük harf yaparak yeni bir dizi oluşturan bir map döngüsü oluşturun.
 
-const arr = ["elma", "armut", "çilek"]
-const upperCase = arr.map((eleman, index) => eleman.toUpperCase() )
-console.log(upperCase);
-console.log(arr);
-//Verilen bir dizinin tüm elemanlarını ekrana yazdıran bir forEach döngüsü oluşturun.
+// const arr = ["elma", "armut", "çilek"]
+// const upperCase = arr.map((eleman, index) => eleman.toUpperCase())
+// console.log(upperCase);
+// console.log(arr);
+// //Verilen bir dizinin tüm elemanlarını ekrana yazdıran bir forEach döngüsü oluşturun.
 
-const arr1 = [1,2,3,4,5,6,7]
+// const arr1 = [1,2,3,4,5,6,7]
 
-const carpimlar = []
+// const carpimlar = []
 
-console.log(arr1.forEach((e) => {
-    carpimlar.push(e * 2)
-}))
+// console.log(arr1.forEach((e) => {
+//     carpimlar.push(e * 2)
+// }))
 
-console.log(carpimlar);
+// console.log(carpimlar);
 
-console.log(arr.map((eleman, index) => eleman.toUpperCase() ));
+// console.log(arr.map((eleman, index) => eleman.toUpperCase() ));
+
+// Kullanılacak diziler
+// const dizi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const dizi1 = ["elma", "armut", "kiraz", "üzüm"];
+// const dizi2 = [1, 2, -3, 4, -5, 6, 7, 8, 9, 10];
+// const dizi3 = [2, 5, 3, 4, 7, 10, 1, 9, 8, 6];
+
+// ---------------forEach-------------------
+// 1- Verilen bir dizinin tüm elemanlarını ekrana yazdıran bir forEach döngüsü oluşturun.
+// 2- Verilen bir dizinin sadece çift sayılarını ekrana yazdıran bir forEach döngüsü oluşturun.
+// 3- Verilen bir dizinin elemanlarını 2 ile çarpan ve sonucu yeni bir diziye ekleyen bir forEach döngüsü oluşturun.
+// 4- Verilen bir dizinin elemanlarının toplamını hesaplayan bir forEach döngüsü oluşturun.(reduce kullanmadan)
+// 5- Verilen bir dizinin elemanlarını ekrana yazdırırken, dizinin son elemanını özel bir mesajla birlikte yazdıran bir forEach döngüsü oluşturun.
+// ---------------map-------------------
+// 6- Verilen bir dizideki elemanları büyük harf yaparak yeni bir dizi oluşturan bir map döngüsü oluşturun.
+// 7- Verilen bir dizinin elemanlarını 1 ile 10 arasında rastgele bir sayıyla toplayıp yeni bir dizi oluşturan bir map döngüsü oluşturun.
+// 8- Verilen bir stringin her bir kelimesini ters çevirerek yeni bir dizi oluşturan bir map döngüsü oluşturun.
+// 9- Verilen bir dizinin elemanlarını(elemanlar sayı olmalı) stringe çevirerek yeni bir dizi oluşturan bir map döngüsü oluşturun.
+// 10- Verilen bir dizi içindeki stringleri ters çevirerek yeni bir dizi oluşturan bir map döngüsü oluşturun. Ancak, bu sefer reverse() yerine kendi ters çevirme algoritmanızı oluşturun.
