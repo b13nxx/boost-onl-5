f6ce9eeedc788ebe897fc36a0680a02a8c4c9f3c
cemmertsimsek <cemsimseksm@gmail.com>
2023-12-04 14:44:31 +0300

    object methods

--- uygulama-icerigi/JS/09-object-methods.js
+++ uygulama-icerigi/JS/09-object-methods.js
@@ -1,10 +1,129 @@
 // 1- myObject isimli boş bir nesne oluşturun.
+const myObject = {}
 // 2- nesnenin içerisine isim, yas, cinsiyet ve meslek özelliklerini sırasıyla ekleyin
+myObject.isim = "Cem"
+myObject.yas = 30
+myObject.cinsiyet = "Erkek"
+myObject.meslek = "Web Developer"
+console.log(myObject);
+
 // 3- nesnenin içerisinden cinsiyet özelliğini kaldırın.
+delete myObject.cinsiyet
 // 4- myObject nesnesinin özelliklerini bir başka değişkene kopyalayın.
+const copyOfMyObject = Object.assign({}, myObject)
+console.log(copyOfMyObject);
+console.log(myObject);
+
+// ----- veya ------
+// const copyOfMyObject2 = {}
+// Object.assign(copyOfMyObject2, myObject)
+
+const target = {a:1, b:2}
+// const source = {b:3 , c:4}
+// const assigned = Object.assign({}, target, source)
+// console.log(assigned);
+
+const newObject = {...myObject}
+console.log(newObject);
+console.log(copyOfMyObject);
+const newObject2 = {...myObject, ...target}
+console.log(newObject2);
+
+
 // 5- nesnenizin anahtarlarını konsola yazdırın.
 // 6- nesnenizin değerlerini konsola yazdırın.
 // 7- nesnenizin tüm anahtar-değer çiftlerini konsola yazdırın.
 // 8- nesnenizin içerisinde isim anahtarına sahip bir özellik var mı diye kontrol edin
 // 9- nesnenizin özelliklerine değişiklik ve ekleme çıkarma yapılabilmesini engelleyin.
 // 10- nesnenizin özelliklerinde değişiklik yapılabilsin ancak ekleme çıkarma yapılabilmesini engelleyin.
+
+// aşağıda verilen aracBilgileri nesnesi içerisinde bulunan her bir servis kayıdına ait tarih bilgisi ve ilgili servis kayıdına ait detay kısmında bulunan aciklama ve ucret bilgilerini konsola yazdırın.
+/*(örn çıktı: 
+05.01.2020 balata değişimi - 1700 TL
+05.01.2020 yağ değişimi - 700 TL
+05.01.2020 boya koruma - 700 TL
+...
+...
+...
+28.08.2022 seramik kaplama - 4500 TL
+)
+*/
+
+const aracBilgileri = {
+    id: "audiA6_123",
+    model: "audiA6",
+    yil: 2019,
+    renk: "kırmızı",
+    servisKayitlari: [
+      {
+        id: "audiA6_123_1",
+        tarih: "05.01.2020",
+        km: 2500,
+        ucret: 3000,
+        detay: [
+          {
+            id: "audiA6_123_1_1",
+            aciklama: "balata değişimi",
+            ucret: 1700,
+          },
+          {
+            id: "audiA6_123_1_2",
+            aciklama: "yağ değişimi",
+            ucret: 700,
+          },
+          {
+            id: "audiA6_123_1_3",
+            aciklama: "boya koruma",
+            ucret: 600,
+          },
+        ],
+      },
+      {
+        id: "audiA6_123_2",
+        tarih: "25.06.2021",
+        km: 10500,
+        ucret: 5000,
+        detay: [
+          {
+            id: "audiA6_123_2_1",
+            aciklama: "balata değişimi",
+            ucret: 2700,
+          },
+          {
+            id: "audiA6_123_2_2",
+            aciklama: "yağ değişimi",
+            ucret: 1700,
+          },
+          {
+            id: "audiA6_123_2_3",
+            aciklama: "boya koruma",
+            ucret: 600,
+          },
+        ],
+      },
+      {
+        id: "audiA6_123_3",
+        tarih: "28.08.2022",
+        km: 25400,
+        ucret: 10000,
+        detay: [
+          {
+            id: "audiA6_123_3_1",
+            aciklama: "balata değişimi",
+            ucret: 3500,
+          },
+          {
+            id: "audiA6_123_3_2",
+            aciklama: "yağ değişimi",
+            ucret: 2000,
+          },
+          {
+            id: "audiA6_123_3_3",
+            aciklama: "seramik kaplama",
+            ucret: 4500,
+          },
+        ],
+      },
+    ],
+  };
+
