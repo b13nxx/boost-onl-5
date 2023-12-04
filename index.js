59ac7c0ba941b7197db32260fcb8ff45c67e810e
Bahrican Akkoyun <bahricanakkoyun@gmail.com>
2023-12-04 12:09:56 +0300

    add async await notes

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // your playground
 
+import fs from 'fs/promises'
 
+;(async () => {
+  try {
+    const promiseOne = fs.readFile('file1.txt', 'utf8') // 2s
+    const promiseTwo = fs.readFile('file2.txt', 'utf8') // 5s
+  
+    const contentOne = await promiseOne
+    const contentTwo = await promiseTwo
+  
+    const contentThree = await fs.readFile('file3.txt', 'utf8')
+  
+    console.log('contentOne', contentOne)
+    console.log('contentTwo', contentTwo)
+    console.log('contentThree', contentThree)
+  } catch (e) {
+    console.log('error', e)
+  }
+
+  console.log('program running')
+})()
--- lessons/async/step3.js
+++ lessons/async/step3.js
@@ -1,3 +1,5 @@
+import fs from 'fs/promises'
+
 // Async / Await
 
 // Async = Asenkron fonksiyonlar oluşturmanızı ve
@@ -13,5 +15,65 @@
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
   const data = await response.json()
 
+  // Veya parantezleri kullanıp işlem önceliği oluşturarak tek satırda yazabiliriz:
+  const result = await (await fetch('https://jsonplaceholder.typicode.com/todos/1')).json()
+
   console.log(data)
+  console.log(result)
+})()
+
+// Asenkron metodları seri çalıştırabiliriz:
+
+;(async () => {
+  const fileOneContent = await fs.readFile('file1.txt', 'utf8')
+
+  console.log('fileOneContent', fileOneContent)
+
+  const fileTwoContent = await fs.readFile('file2.txt', 'utf8')
+
+  console.log('fileTwoContent', fileTwoContent)
+
+  const fileThreeContent = await fs.readFile('file3.txt', 'utf8')
+
+  console.log('fileThreeContent', fileThreeContent)
+})()
+
+
+// Asenkron metodları paralel de çalışıyoruz:
+
+;(async () => {
+  const promiseOne = fs.readFile('file1.txt', 'utf8') // 2s
+  const promiseTwo = fs.readFile('file2.txt', 'utf8') // 5s
+  const promiseThree = fs.readFile('file3.txt', 'utf8') // 3s
+
+  const contentOne = await promiseOne
+  const contentTwo = await promiseTwo
+  const contentThree = await promiseThree
+
+  console.log('contentOne', contentOne)
+  console.log('contentTwo', contentTwo)
+  console.log('contentThree', contentThree)
+})()
+
+
+// Seri ve paralel süreçleri combine edebiliriz:
+
+;(async () => {
+  try {  // Promise.catch yapısı Try Catch bloklarıyla oluşturabiliriz
+    const promiseOne = fs.readFile('file1.txt', 'utf8') // 2s
+    const promiseTwo = fs.readFile('file2.txt', 'utf8') // 5s
+  
+    const contentOne = await promiseOne
+    const contentTwo = await promiseTwo
+  
+    const contentThree = await fs.readFile('file3.txt', 'utf8')
+  
+    console.log('contentOne', contentOne)
+    console.log('contentTwo', contentTwo)
+    console.log('contentThree', contentThree)
+  } catch (e) {
+    console.log('error', e)
+  }
+
+  console.log('program running')
 })()
