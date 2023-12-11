1cdcb3187ce2a37aa5e49be9df871129ea908a59
cemmertsimsek <cemsimseksm@gmail.com>
2023-12-11 17:09:05 +0300

    json-server axios useState-useEffect

--- uygulama-icerigi/REACT/recipe-app/src/App.jsx
+++ uygulama-icerigi/REACT/recipe-app/src/App.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import NewRecipeForm from "./components/NewRecipeForm";
 import RecipeList from "./components/RecipeList";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/recipes")
+      .then((response) => setRecipes(response.data))
+      .catch((error) => {
+        console.log("There was an error while fetching the recipes");
+      });
+  }, []);
 
+  const addRecipeToList = (newRecipe) => {
+    setRecipes((prevRecipes) => [...prevRecipes,newRecipe])
+  }
 
   return (
     <>
       <Header />
       <Home />
-      <NewRecipeForm/>
-      <RecipeList  />
+      <NewRecipeForm addRecipeToList={addRecipeToList} />
+      <RecipeList recipes={recipes}/>
     </>
   );
 }
--- uygulama-icerigi/REACT/recipe-app/src/components/NewRecipeForm.jsx
+++ uygulama-icerigi/REACT/recipe-app/src/components/NewRecipeForm.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-const NewRecipeForm = () => {
+const NewRecipeForm = ({addRecipeToList}) => {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log("Submitting new recipe:");
 
-        setTitle('')
-    }
 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/recipes", { title, description, image })
+      .then((response) => {
+        console.log(response.data);
+        addRecipeToList(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error posting the new recipe!", error);
+      });
+
+    setTitle("");
+    setDescription("")
+    setImage("")
+  };
 
   return (
-    <form onSubmit={handleSubmit} >
+    <form onSubmit={handleSubmit}>
       <input
         value={title}
         type="text"
@@ -20,8 +35,19 @@ const NewRecipeForm = () => {
         required
         onChange={(event) => setTitle(event.target.value)}
       />
-      <textarea placeholder="Recipe Description" required />
-      <input type="text" placeholder="Image URL" required />
+      <textarea
+        value={description}
+        placeholder="Recipe Description"
+        onChange={(event) => setDescription(event.target.value)}
+        required
+      />
+      <input
+        value={image}
+        type="text"
+        placeholder="Image URL"
+        onChange={(event) => setImage(event.target.value)}
+        required
+      />
       <button type="submit">Add Recipe</button>
     </form>
   );
